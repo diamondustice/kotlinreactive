c8be7ec47af2f80ab3fc2fcaf51740e149262230
Eiselle Sok <mails@eiselle.com>
2023-10-12 03:59:50 -0500

    Cleanup instance variables naming

--- src/main/kotlin/com/eiselle/kotlinreactive/dto/WeatherDay.kt
+++ src/main/kotlin/com/eiselle/kotlinreactive/dto/WeatherDay.kt
@@ -3,22 +3,22 @@ package com.eiselle.kotlinreactive.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class WeatherDay(
-        private val day_name: String,
-        private val temp_high_celsius: String,
-        private val forecast_blurp: String
+        private val dayName: String,
+        private val tempHighCelsius: String,
+        private val forecastBlurp: String
 ) {
     @JsonProperty("day_name")
     fun getDayName(): String {
-        return day_name
+        return dayName
     }
 
     @JsonProperty("temp_high_celsius")
     fun getTempHighCelsius(): String {
-        return temp_high_celsius
+        return tempHighCelsius
     }
 
     @JsonProperty("forecast_blurp")
     fun getForecastBlurp(): String {
-        return forecast_blurp
+        return forecastBlurp
     }
 }
